82675f609bcee3019522a5c69c76e545559dafa9
Sammy Levin <sammylevin@macbook-pro-sl.myfiosgateway.com>
2020-11-24 14:28:35 -0500

    Routes added

--- src/containers/CreateAccount.js
+++ src/containers/CreateAccount.js
@@ -1,7 +1,11 @@
 import React from "react";
 
 function CreateAccount() {
-  return <div></div>;
+  return (
+    <div>
+      <h1>Create Account</h1>
+    </div>
+  );
 }
 
 export default CreateAccount;
